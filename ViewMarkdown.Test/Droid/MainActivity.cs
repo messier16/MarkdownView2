7641d88f5a04f25d60ecf1706b8c0f01e9e43b1a
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-06-06 21:13:30 +0100

    Android project (no references)

---
+++ ViewMarkdown.Test/Droid/MainActivity.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+
+namespace ViewMarkdown.Test.Droid
+{
+	[Activity(Label = "ViewMarkdown.Test.Droid", Icon = "@mipmap/ic_launcher", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			//TabLayoutResource = Resource.Layout.Tabbar;
+			//ToolbarResource = Resource.Layout.Toolbar;
+
+			base.OnCreate(bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
+			//MarkdownViewRenderer.Init();
+
+			//LoadApplication(new App());
+		}
+	}
+}
+
