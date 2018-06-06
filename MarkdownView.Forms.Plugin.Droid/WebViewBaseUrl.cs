de55d82b99be15521d6c026d320a2c42c67cb231
Antonio Feregrino <antonio.feregrino@gmail.com>
2018-06-06 10:08:22 +0100

    Android project.

---
+++ MarkdownView.Forms.Plugin.Droid/WebViewBaseUrl.cs
@@ -0,0 +1,21 @@
+﻿using ViewMarkdown.Forms.Plugin.Abstractions;
+using ViewMarkdown.Forms.Plugin.Droid;
+using Xamarin.Forms;
+
+
+[assembly: Dependency(typeof(WebViewBaseUrl))]
+namespace ViewMarkdown.Forms.Plugin.Droid
+{
+	public class WebViewBaseUrl : IWebViewBaseUrl
+	{
+		#region IWebViewBaseUrl implementation
+		string IWebViewBaseUrl.Url {
+			get {
+				return "file:///android_asset/";
+			}
+		}
+		#endregion
+		
+	}
+}
+
